060c4a5b28aa7f2be2684ae1fac2c487f6f5fb5d
Liam Kerr <LiamKerr42@outlook.com>
2026-06-07 17:07:16 +1200

    work on server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 
+import { Pool } from "pg";
+
+export const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+});
+
 const app = express();
 
 app.use(cors());
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,25 @@
+import { quantico } from "./components/fonts"; 
+
 export default function HomePage() {
 return (
     <main>
-        
+        <div className="bg-mauve-900 p-5">
+            <h2 className={`${quantico.className} text-left font-bold underline text-4xl`}>
+                Latest Scores
+            </h2>
+        </div>
+
+        <div className="bg-mauve-900 p-5">
+            <h2 className={`${quantico.className} text-left font-bold underline text-4xl`}>
+                Trending News
+            </h2>
+        </div>
+
+        <div className="bg-mauve-900 p-5">
+            <h2 className={`${quantico.className} text-left font-bold underline text-4xl`}>
+                Recommended For You
+            </h2>
+        </div>
     </main>
 );
 }
